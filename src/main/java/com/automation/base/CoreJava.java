1a404b616e7a61adbc6f04b6ba305f05ce671c41
ajaajahmad <ajaaj75@gmail.com>
2025-08-11 23:44:03 +0530

    Initial commit

---
+++ src/main/java/com/automation/base/CoreJava.java
@@ -0,0 +1,14 @@
+package com.automation.base;
+
+public class CoreJava {
+
+	public static void main(String[] args) {
+
+		int myNum = 5;
+		String myWebsite = "www.example.com";
+		char myFlag = 'A';
+		float myDeci = 5.99f;
+		boolean isTrue = false;
+	}
+
+}
--- src/main/java/com/automation/base/InvokeBrowser.java
+++ src/main/java/com/automation/base/InvokeBrowser.java
@@ -1,12 +1,17 @@
 package com.automation.base;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class InvokeBrowser {
 
 	public static void main(String[] args) {
-
-		ChromeDriver driver = new ChromeDriver();
+		
+		//WebDriver driver = new EdgeDriver();
+		//WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://www.rahulshettyacademy.com/");
 		System.out.println(driver.getTitle());
