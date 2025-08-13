4a3a18ee415823a3d23ab50905ed1a9ad8897745
ajaajahmad <ajaaj75@gmail.com>
2025-08-13 15:00:16 +0530

    locators

---
+++ src/main/java/com/automation/base/Arralist.java
@@ -0,0 +1,32 @@
+package com.automation.base;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arralist {
+
+	public static void main(String[] args) {
+		ArrayList<String> a = new ArrayList<>();
+		
+		a.add("Ajaaj");
+		a.add("Ahmad");
+		a.add("Sheikh");
+		a.add("Null");
+		
+		System.out.println(a.get(0));
+		
+		
+		List<Integer> b = new ArrayList<>();
+		
+		b.add(1);
+		b.add(2);
+		b.add(3);
+		b.add(4);
+		b.add(5);
+		b.add(6);
+		b.remove(5);
+		
+		System.out.println(b.get(4));
+	}
+
+}
