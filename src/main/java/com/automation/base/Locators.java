7424ab1b962feb024d2021ac6a376ec4be51c6c0
ajaajahmad <ajaaj75@gmail.com>
2025-08-13 22:15:22 +0530

    dynamic locators

--- src/main/java/com/automation/base/Locators.java
+++ src/main/java/com/automation/base/Locators.java
@@ -20,7 +20,17 @@ public class Locators {
 		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
 		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@doamin.com");
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
+//		driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
+		driver.findElement(By.xpath("//form//input[3]")).sendKeys("9090909090");
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+		driver.findElement(By.xpath("//button[contains(@class,'go-to-login-btn')]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+		driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.id("chkboxOne")).click();
+		driver.findElement(By.id("chkboxTwo")).click();
+		driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
 		Thread.sleep(2000);
 		driver.quit();
 	}
