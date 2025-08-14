497b7753802bb8b3ce0be59965cf32e458445258
ajaajahmad <ajaaj75@gmail.com>
2025-08-14 23:28:08 +0530

    StaticDropDown

--- src/main/java/com/automation/base/StaticDropDown.java
+++ src/main/java/com/automation/base/StaticDropDown.java
@@ -5,15 +5,13 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class BroserActivities {
+public class StaticDropDown {
 
 	public static void main(String[] args) {
+		
 		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		driver.get("https://www.google.com");
-		driver.navigate().to("https://www.fb.com");
-		driver.navigate().back();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 	}
 
 }
