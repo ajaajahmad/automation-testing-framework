0dc3954ba98cac70c3d4f7fd4278585e3573e580
ajaajahmad <ajaaj75@gmail.com>
2025-08-14 23:32:29 +0530

    StaticDropDown

--- src/main/java/com/automation/base/StaticDropDown.java
+++ src/main/java/com/automation/base/StaticDropDown.java
@@ -2,16 +2,25 @@ package com.automation.base;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class StaticDropDown {
 
 	public static void main(String[] args) {
-		
+
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		WebElement staticDropDown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+		Select dropDown = new Select(staticDropDown);
+		dropDown.selectByIndex(3);
+		System.out.println(dropDown.getFirstSelectedOption().getText());
+		driver.quit();
 	}
 
 }
