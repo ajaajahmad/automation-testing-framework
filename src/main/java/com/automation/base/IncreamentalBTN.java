94838a7be7d5d058f1dab836aa0a68855c5fa177
ajaajahmad <ajaaj75@gmail.com>
2025-08-18 23:23:06 +0530

    Static Dropdown

---
+++ src/main/java/com/automation/base/IncreamentalBTN.java
@@ -0,0 +1,21 @@
+package com.automation.base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IncreamentalBTN {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://www.spicejet.com/");
+		
+		driver.findElement(By.xpath("//div[text()='Passengers']")).click();
+	}
+
+}
