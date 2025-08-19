05d665f6ee9d6b2f9def870a2e3ebb4ee1390371
ajaajahmad <ajaaj75@gmail.com>
2025-08-19 23:41:04 +0530

    Increamental Button

--- src/main/java/com/automation/base/IncreamentalBTN.java
+++ src/main/java/com/automation/base/IncreamentalBTN.java
@@ -27,6 +27,8 @@ public class IncreamentalBTN {
 			driver.findElement(By.xpath("//*[@data-testid='Adult-testID-plus-one-cta']")).click();
 			click++;
 		}
+		
+		System.out.println(driver.findElement(By.xpath("//div[@class='css-1dbjc4n']//div//div[@class='css-1dbjc4n r-14lw9ot r-11u4nky r-z2wwpe r-1phboty r-rs99b7 r-1loqt21 r-13awgt0 r-ymttw5 r-5njf8e r-1otgn73']//div[@class='css-1dbjc4n r-1awozwy r-18u37iz r-1wtj0ep']")).getText());
 
 		driver.quit();
 	}
