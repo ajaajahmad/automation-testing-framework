74ecc2cf53eb1ddf8df82afd5078cff2ba3cf76a
ajaajahmad <ajaaj75@gmail.com>
2025-08-20 00:35:53 +0530

    Dynamic DropDown

---
+++ src/main/java/com/automation/base/DynamicDropDown.java
@@ -0,0 +1,27 @@
+package com.automation.base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DynamicDropDown {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+		driver.findElement(By.cssSelector("#ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[@value='MAA']")).click();
+		
+		driver.findElement(By.xpath("//a[@value='BLR']")).click();
+
+		driver.quit();
+	}
+
+}
