e67c11a84eadcfff7b7b36ed8fc8af3e37b4ddff
ajaajahmad <ajaaj75@gmail.com>
2025-08-19 23:36:37 +0530

    Increamental Button

--- src/main/java/com/automation/base/IncreamentalBTN.java
+++ src/main/java/com/automation/base/IncreamentalBTN.java
@@ -9,13 +9,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class IncreamentalBTN {
 
 	public static void main(String[] args) {
-		
+
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://www.spicejet.com/");
-		
+
 		driver.findElement(By.xpath("//div[text()='Passengers']")).click();
+
+		for (int click = 1; click < 5; click++) {
+			driver.findElement(By.xpath("//*[@data-testid='Adult-testID-plus-one-cta']")).click();
+		}
+
+		driver.quit();
 	}
 
 }
