b391a79f9dd4e90753bb449cf58ebdf508c830f5
ajaajahmad <ajaaj75@gmail.com>
2025-08-23 14:52:53 +0530

    HandleCheckBoxes

--- src/main/java/com/automation/base/ArrList.java
+++ src/main/java/com/automation/base/ArrList.java
@@ -3,7 +3,7 @@ package com.automation.base;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Arralist {
+public class ArrList {
 
 	public static void main(String[] args) {
 		ArrayList<String> a = new ArrayList<>();
