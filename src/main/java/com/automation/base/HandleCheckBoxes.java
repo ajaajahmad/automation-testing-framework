5df3e1326c495751eacc7dc40d9565d5ea8ea04b
ajaajahmad <ajaaj75@gmail.com>
2025-08-23 14:17:20 +0530

    HandleCheckBoxes

--- src/main/java/com/automation/base/HandleCheckBoxes.java
+++ src/main/java/com/automation/base/HandleCheckBoxes.java
@@ -15,8 +15,10 @@ public class HandleCheckBoxes {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
 		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
-		
+		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+
 		Thread.sleep(2000);
 
 		driver.quit();
