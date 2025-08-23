bebf00bd3adf82288954b80601d2c1bc1864806f
ajaajahmad <ajaaj75@gmail.com>
2025-08-23 14:14:43 +0530

    HandleCheckBoxes

---
+++ src/main/java/com/automation/base/HandleCheckBoxes.java
@@ -0,0 +1,26 @@
+package com.automation.base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleCheckBoxes {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+		
+		Thread.sleep(2000);
+
+		driver.quit();
+
+	}
+
+}
