d0f60c1165593463fce40ab0fba05a509efcae73
ajaajahmad <ajaaj75@gmail.com>
2025-08-24 16:04:24 +0530

    Handle Date Picker

---
+++ src/main/java/com/automation/base/HandleDatePicker.java
@@ -0,0 +1,30 @@
+package com.automation.base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleDatePicker {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.findElement(By.cssSelector("#ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='MAA']"))
+				.click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='BLR']"))
+				.click();
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector(".ui-state-default.ui-state-active")).click();
+		Thread.sleep(3000);
+		driver.quit();
+	}
+
+}
