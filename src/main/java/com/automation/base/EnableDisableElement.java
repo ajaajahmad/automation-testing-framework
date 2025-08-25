90c9a46e3da0879b6bc4ee9b37627480a7f5d2c6
ajaajahmad <ajaaj75@gmail.com>
2025-08-25 23:53:34 +0530

    Handle Date Picker

--- src/main/java/com/automation/base/EnableDisableElement.java
+++ src/main/java/com/automation/base/EnableDisableElement.java
@@ -21,7 +21,7 @@ public class EnableDisableElement {
 		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
 		// System.out.println(driver.findElement(By.name("ctl00_mainContent_view_date2")).isEnabled());
 		System.out.println(driver.findElement(By.id("Div1")).getDomAttribute("style"));
-		if (driver.findElement(By.id("Div1")).getDomAttribute("style").contains("0.5")) {
+		if (driver.findElement(By.id("Div1")).getDomAttribute("style").contains("1")) {
 			System.out.println("It's enabled");
 			Assert.assertTrue(true);
 		} else {
