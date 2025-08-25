e486fe3804bbec3f0cdff6364721520043f969ed
ajaajahmad <ajaaj75@gmail.com>
2025-08-25 23:28:23 +0530

    Handle Date Picker

---
+++ src/main/java/com/automation/base/EnableDisableElement.java
@@ -0,0 +1,26 @@
+package com.automation.base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EnableDisableElement {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+		System.out.println(driver.findElement(By.name("ctl00_mainContent_view_date2")).isEnabled());
+		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+		System.out.println(driver.findElement(By.name("ctl00_mainContent_view_date2")).isEnabled());
+
+		driver.quit();
+	}
+
+}
