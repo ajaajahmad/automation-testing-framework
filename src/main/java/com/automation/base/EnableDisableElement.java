2382c89c583cf0daa338e372d9085232a1cc3f97
ajaajahmad <ajaaj75@gmail.com>
2025-08-25 23:45:39 +0530

    Handle Date Picker

--- src/main/java/com/automation/base/EnableDisableElement.java
+++ src/main/java/com/automation/base/EnableDisableElement.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class EnableDisableElement {
 
@@ -16,10 +17,16 @@ public class EnableDisableElement {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
-		//System.out.println(driver.findElement(By.name("ctl00_mainContent_view_date2")).isEnabled());
+		// System.out.println(driver.findElement(By.name("ctl00_mainContent_view_date2")).isEnabled());
 		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
-		//System.out.println(driver.findElement(By.name("ctl00_mainContent_view_date2")).isEnabled());
-
+		// System.out.println(driver.findElement(By.name("ctl00_mainContent_view_date2")).isEnabled());
+		System.out.println(driver.findElement(By.id("Div1")).getDomAttribute("style"));
+		if (driver.findElement(By.id("Div1")).getDomAttribute("style").contains("1"))
+			;
+		{
+			System.out.println("It's enabled");
+			Assert.assertTrue(true);
+		}
 		driver.quit();
 	}
 
