1ec7e700978a83e055a001369d2b3554d30ca7a7
ajaajahmad <ajaaj75@gmail.com>
2025-08-26 22:37:41 +0530

    End To End Flow

---
+++ src/main/java/com/automation/base/EndToEndFlow.java
@@ -0,0 +1,39 @@
+package com.automation.base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EndToEndFlow {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_0")).click();
+		driver.findElement(By.cssSelector("#ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		Thread.sleep(2000);
+		// driver.findElement(By.xpath("//a[@value='MAA']")).click();
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='DEL']"))
+				.click();
+		// driver.findElement(By.xpath("(//a[@value='BLR'])[2]")).click();
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='BOM']"))
+				.click();
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector(".ui-state-default.ui-state-active")).click();
+
+		driver.findElement(By.xpath("//div[text()='Passengers']")).click();
+
+		for (int click = 1; click < 5; click++) {
+			driver.findElement(By.xpath("//*[@data-testid='Adult-testID-plus-one-cta']")).click();
+		}
+
+		driver.quit();
+	}
+
+}
