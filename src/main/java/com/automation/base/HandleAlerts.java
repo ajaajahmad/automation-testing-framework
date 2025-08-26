99bfc2698b9dbb6afa123027ca2839659abce07f
ajaajahmad <ajaaj75@gmail.com>
2025-08-26 23:03:02 +0530

    Handle Alerts

--- src/main/java/com/automation/base/HandleAlerts.java
+++ src/main/java/com/automation/base/HandleAlerts.java
@@ -10,20 +10,17 @@ public class HandleAlerts {
 
 	public static void main(String[] args) {
 
+		String text = "ajaaj";
+		
 		WebDriver driver = new ChromeDriver();
 
 		driver.manage().window().maximize();
-
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-
-		driver.findElement(By.id("name")).sendKeys("ajaaj");
-
+		driver.findElement(By.id("name")).sendKeys(text);
 		driver.findElement(By.cssSelector("[id='alertbtn']")).click();
-
 		driver.switchTo().alert().accept();
-		
+
 		driver.quit();
 	}
 
