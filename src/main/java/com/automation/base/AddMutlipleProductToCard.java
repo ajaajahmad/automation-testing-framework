52e146ba7fa9e23ee038b46567b51563f39632f8
ajaajahmad <ajaaj75@gmail.com>
2025-08-28 00:25:53 +0530

    Add Multiple Products To Card

--- src/main/java/com/automation/base/AddMutlipleProductToCard.java
+++ src/main/java/com/automation/base/AddMutlipleProductToCard.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AddMutlipleProductToCard {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		int count = 0;
 		String[] item = { "Cucumber", "Brocolli", "Beetroot", "Carrot" };
 
