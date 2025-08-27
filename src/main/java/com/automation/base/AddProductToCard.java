6dc1d6b25b0f4a8a25e3bcec7b414ecb8219a7bb
ajaajahmad <ajaaj75@gmail.com>
2025-08-27 22:37:58 +0530

    Add Product To Card

--- src/main/java/com/automation/base/AddProductToCard.java
+++ src/main/java/com/automation/base/AddProductToCard.java
@@ -1,9 +1,34 @@
 package com.automation.base;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AddProductToCard {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().deleteAllCookies();
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+
+		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+
+		for (int i = 0; i < products.size(); i++) {
+
+			String name = products.get(i).getText();
+			
+			if (name.contains("Cucumber")) {
+				driver.findElement(By.xpath("//button[text{}='ADD TO CART']")).click();
+				break;
+			}
+		}
 
 	}
 
