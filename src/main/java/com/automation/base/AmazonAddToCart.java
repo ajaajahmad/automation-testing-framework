abf8f61b2d8b47feb7e8f8cd6cb3d210117ad4b2
ajaajahmad <ajaaj75@gmail.com>
2025-08-30 00:16:55 +0530

    Amazon Add To Cart

---
+++ src/main/java/com/automation/base/AmazonAddToCart.java
@@ -0,0 +1,27 @@
+package com.automation.base;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AmazonAddToCart {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+		driver.get("https://www.amazon.in/");
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iPhone");
+		List<WebElement> productNameElement = driver.findElements(By.xpath("//span[contains(text()=,'Apple iPhone')]"));
+	}
+
+}
