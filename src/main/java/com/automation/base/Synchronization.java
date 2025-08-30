243fef020a499d9459a92e397a2fa4878b57c4ab
ajaajahmad <ajaaj75@gmail.com>
2025-08-31 01:25:10 +0530

    Synchronization

--- src/main/java/com/automation/base/Synchronization.java
+++ src/main/java/com/automation/base/Synchronization.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Synchronization {
 
@@ -16,19 +18,22 @@ public class Synchronization {
 		String[] requiredProducts = { "Capsicum", "Mushroom", "Pumpkin", "Banana", "Cashews" };
 
 		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		// driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 
 		addItemsToCard(driver, requiredProducts);
 
 		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
 		driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));
 		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='promoWrapper']/button")));
 		driver.findElement(By.xpath("//div[@class='promoWrapper']/button")).click();
-
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
 		System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
 
 		driver.quit();
