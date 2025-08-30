f63588e02a6baee1e2f39996a52b2498f4d2adb6
ajaajahmad <ajaaj75@gmail.com>
2025-08-31 00:45:57 +0530

    Synchronization

--- src/main/java/com/automation/base/Synchronization.java
+++ src/main/java/com/automation/base/Synchronization.java
@@ -3,42 +3,51 @@ package com.automation.base;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Synchronization {
+
 	public static void main(String[] args) {
-		String[] requiredProducts = { "Cucumber", "Broccoli", "Beetroot", "Carrot" };
+
+		String[] requiredProducts = { "Capsicum", "Mushroom", "Pumpkin", "Banana", "Cashews" };
 
 		WebDriver driver = new ChromeDriver();
+
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 
-		addItems(driver, requiredProducts);
-		driver.quit();
+		addItemsToCard(driver, requiredProducts);
+
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+		driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.xpath("//div[@class='promoWrapper']/button")).click();
+
+		// driver.quit();
 	}
 
-	public static void addItems(WebDriver driver, String[] requiredProducts) {
-		List<WebElement> productNameElements = driver.findElements(By.cssSelector("h4.product-name"));
-		List<String> requiredProductsList = Arrays.asList(requiredProducts);
+	public static void addItemsToCard(WebDriver driver, String[] requiredProducts) {
+
 		int matchedProductsCount = 0;
 
+		List<WebElement> productNameElements = driver.findElements(By.cssSelector("h4.product-name"));
+
+		List<String> requiredProductList = Arrays.asList(requiredProducts);
+
 		for (int i = 0; i < productNameElements.size(); i++) {
-			String[] productTextParts = productNameElements.get(i).getText().split("-");
+			String[] productNameParts = productNameElements.get(i).getText().split("-");
+			String productName = productNameParts[0].trim();
 
-			// Format originalName to get requiredName
-			String productName = productTextParts[0].trim();
-
-			// Check if product name is in the required products list
-			if (requiredProductsList.contains(productName)) {
+			if (requiredProductList.contains(productName)) {
 				matchedProductsCount++;
-
-				// Click the add to cart button for the matching product
-				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+				driver.findElements(By.xpath("//*[@class='product-action']/button")).get(i).click();
 
 				if (matchedProductsCount == requiredProducts.length) {
 					break;
