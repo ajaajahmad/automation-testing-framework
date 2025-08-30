51f2d92e37a3addb90e09719eb701ad10ceede89
ajaajahmad <ajaaj75@gmail.com>
2025-08-30 23:46:26 +0530

    Synchronization

--- src/main/java/com/automation/base/AddMutlipleProductToCart.java
+++ src/main/java/com/automation/base/AddMutlipleProductToCart.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class AddMutlipleProductToCard {
+public class AddMutlipleProductToCart {
 
 	public static void main(String[] args) {
 		int count = 0;
--- src/main/java/com/automation/base/AddOneProductToCart.java
+++ src/main/java/com/automation/base/AddOneProductToCart.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class AddOneProductToCard {
+public class AddOneProductToCart {
 
 	public static void main(String[] args) {
 
