097ef1ea95dead4a196d86100ca0ca954420ea15
ajaajahmad <ajaaj75@gmail.com>
2025-09-02 23:32:39 +0530

    Java Script Executor

--- src/main/java/com/automation/base/JavaScriptExecutor.java
+++ src/main/java/com/automation/base/JavaScriptExecutor.java
@@ -1,5 +1,6 @@
 package com.automation.base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,6 +20,8 @@ public class JavaScriptExecutor {
 		js.executeScript("window.scrollBy(0,500)");
 		Thread.sleep(3000);
 		js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
+
+		driver.findElement(By.cssSelector(".tableFixHead td:nth-child(4)"));
 	}
 
 }
