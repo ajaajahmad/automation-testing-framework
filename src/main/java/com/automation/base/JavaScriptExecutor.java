4516b21a417ffdba4cf8273789bfb37e62261b18
ajaajahmad <ajaaj75@gmail.com>
2025-09-02 23:12:16 +0530

    Java Script Executor

---
+++ src/main/java/com/automation/base/JavaScriptExecutor.java
@@ -0,0 +1,18 @@
+package com.automation.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaScriptExecutor {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		JavaScriptExecutor js = (JavaScriptExecutor)driver;
+	}
+
+}
