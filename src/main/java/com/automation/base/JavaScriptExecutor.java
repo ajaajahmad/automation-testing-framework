5b89d805c0228c0db496c7c77e4b393adae32b23
ajaajahmad <ajaaj75@gmail.com>
2025-09-02 23:26:10 +0530

    Java Script Executor

--- src/main/java/com/automation/base/JavaScriptExecutor.java
+++ src/main/java/com/automation/base/JavaScriptExecutor.java
@@ -1,18 +1,24 @@
 package com.automation.base;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class JavaScriptExecutor {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws InterruptedException {
+
 		WebDriver driver = new ChromeDriver();
-		
+
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		
-		JavaScriptExecutor js = (JavaScriptExecutor)driver;
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		js.executeScript("window.scrollBy(0,500)");
+		Thread.sleep(3000);
+		js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
 	}
 
 }
