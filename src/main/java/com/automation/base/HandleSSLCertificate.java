b23dbee4d74193a223575f2a335d1d9a573d4f7e
ajaajahmad <ajaaj75@gmail.com>
2025-09-04 14:46:35 +0530

    Handle SSL Certificate

--- src/main/java/com/automation/base/HandleSSLCertificate.java
+++ src/main/java/com/automation/base/HandleSSLCertificate.java
@@ -1,9 +1,15 @@
 package com.automation.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class HandleSSLCertificate {
 
 	public static void main(String[] args) {
-		
+			
+		WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
 	}
 
 }
