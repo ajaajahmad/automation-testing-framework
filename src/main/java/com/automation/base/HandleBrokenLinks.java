08e48cd98c98670c61c70ca2e6044e07dadea94d
ajaajahmad <ajaaj75@gmail.com>
2025-09-05 09:35:29 +0530

    Take Screenshot

--- src/main/java/com/automation/base/HandleBrokenLinks.java
+++ src/main/java/com/automation/base/HandleBrokenLinks.java
@@ -1,10 +1,19 @@
 package com.automation.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HandleBrokenLinks {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://expired.badssl.com/");
+		
+		
 	}
 
 }
