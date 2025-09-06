9a2122342053a87b574c822d5fbd0336f35a3f47
ajaajahmad <ajaaj75@gmail.com>
2025-09-06 22:40:21 +0530

    Handle Broken Links

--- src/main/java/com/automation/base/HandleBrokenLinks.java
+++ src/main/java/com/automation/base/HandleBrokenLinks.java
@@ -1,19 +1,32 @@
 package com.automation.base;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HandleBrokenLinks {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws MalformedURLException, IOException {
+
 		WebDriver driver = new ChromeDriver();
-		
+
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-		
-		
+
+		String urlsElement = driver.findElement(By.cssSelector("a[href*='brokenlink']")).getAttribute("href");
+
+		HttpURLConnection connection = (HttpURLConnection) new URL(urlsElement).openConnection();
+		connection.setRequestMethod("Head");
+		connection.connect();
+		int responseCode = connection.getResponseCode();
+		System.out.println(responseCode);
+
 	}
 
 }
