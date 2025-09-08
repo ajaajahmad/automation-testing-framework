9b57310c35e4429180bbfb29bdb36ac42285f9af
ajaajahmad <ajaaj75@gmail.com>
2025-09-08 13:31:05 +0530

    Relative XPath in Selenium 4.x

---
+++ src/main/java/com/automation/base/RelativeXPath.java
@@ -0,0 +1,19 @@
+package com.automation.base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator.*;
+
+public class RelativeXPath {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		WebElement nameText = driver.findElement(By.cssSelector(""));
+		System.out.println(driver.findElement(with(By.tagName("label")).above(nameText)).getText());
+	}
+
+}
