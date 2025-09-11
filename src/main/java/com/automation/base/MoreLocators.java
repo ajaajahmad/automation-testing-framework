ea101575039ebd8a833aff2a2b6144a767ae6931
ajaajahmad <ajaaj75@gmail.com>
2025-09-11 23:26:22 +0530

    code optimization

--- src/main/java/com/automation/base/MoreLocators.java
+++ src/main/java/com/automation/base/MoreLocators.java
@@ -1,7 +1,6 @@
 package com.automation.base;
 
 import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,13 +8,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class MoreLocators {
 
 	public static void main(String[] args) {
-		
+
 		WebDriver driver = new ChromeDriver();
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+
+		System.out
+				.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
 		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText());
+
 		driver.quit();
 	}
 
