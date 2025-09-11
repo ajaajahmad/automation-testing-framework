0902ef37c2309fa968100bde25536bd7ce260237
ajaajahmad <ajaaj75@gmail.com>
2025-09-11 23:24:03 +0530

    code optimization

--- src/main/java/com/automation/base/StaticDropDown.java
+++ src/main/java/com/automation/base/StaticDropDown.java
@@ -13,16 +13,25 @@ public class StaticDropDown {
 	public static void main(String[] args) {
 
 		WebDriver driver = new ChromeDriver();
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
 		WebElement staticDropDown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+
 		Select dropDown = new Select(staticDropDown);
+
 		dropDown.selectByIndex(3);
+
 		System.out.println(dropDown.getFirstSelectedOption().getText());
+
 		dropDown.selectByVisibleText("AED");
+
 		System.out.println(dropDown.getFirstSelectedOption().getText());
+
 		dropDown.selectByValue("INR");
+
 		System.out.println(dropDown.getFirstSelectedOption().getText());
 
 		driver.quit();
