958df1a8d6cf082f56b856803eab2a6309254df8
ajaajahmad <ajaaj75@gmail.com>
2025-09-14 23:42:10 +0530

    code optimization

--- src/main/java/com/automation/base/InvokeBrowser.java
+++ src/main/java/com/automation/base/InvokeBrowser.java
@@ -2,15 +2,13 @@ package com.automation.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class InvokeBrowser {
 
 	public static void main(String[] args) {
-		
-		//WebDriver driver = new EdgeDriver();
-		//WebDriver driver = new FirefoxDriver();
+
+		// WebDriver driver = new EdgeDriver();
+		// WebDriver driver = new FirefoxDriver();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://www.rahulshettyacademy.com/");
