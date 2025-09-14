fe3bdc2ff0f18c42c0a26cd6adc48c51f330c339
ajaajahmad <ajaaj75@gmail.com>
2025-09-14 23:41:17 +0530

    code optimization

--- src/main/java/com/automation/base/HandleBrokenLinks.java
+++ src/main/java/com/automation/base/HandleBrokenLinks.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 
 public class HandleBrokenLinks {
