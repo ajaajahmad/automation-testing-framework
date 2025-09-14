361ed8e4e0da852b21f10ec0fa11c1aacbd6c4be
ajaajahmad <ajaaj75@gmail.com>
2025-09-14 23:40:29 +0530

    code optimization

--- src/main/java/com/automation/base/FacebookSingup.java
+++ src/main/java/com/automation/base/FacebookSingup.java
@@ -15,23 +15,22 @@ public class FacebookSingup {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://www.facebook.com/r.php?entry_point=login");
-		
+
 		WebElement staticElementDay = driver.findElement(By.id("day"));
 		WebElement staticElementMonth = driver.findElement(By.id("month"));
 		WebElement staticElementYear = driver.findElement(By.id("year"));
-		
+
 		Select dropDownDay = new Select(staticElementDay);
 		dropDownDay.selectByVisibleText("10");
-		
+
 		Select dropDownMonth = new Select(staticElementMonth);
 		dropDownMonth.selectByVisibleText("Jul");
-		
-		
+
 		Select dropDownYear = new Select(staticElementYear);
 		dropDownYear.selectByVisibleText("2000");
-		
+
 		driver.quit();
-		
+
 	}
 
 }
