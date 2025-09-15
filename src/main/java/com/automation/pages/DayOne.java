9688c2db4a6b737817a9d979ced6f14d73ed1e0e
ajaajahmad <ajaaj75@gmail.com>
2025-09-15 23:28:16 +0530

    Initial TestNG Setup

---
+++ src/main/java/com/automation/pages/DayOne.java
@@ -0,0 +1,16 @@
+package com.automation.pages;
+
+import org.testng.annotations.Test;
+
+public class DayOne {
+	
+	@Test
+	public void firstTest() {
+		System.out.println("1");
+	}
+
+	@Test
+	public void secondTest() {
+		System.out.println("2");
+	}
+}
