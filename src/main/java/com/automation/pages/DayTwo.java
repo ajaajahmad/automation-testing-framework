89bb76fd1fd80bc8ce7c88065595dae6a75fa77d
ajaajahmad <ajaaj75@gmail.com>
2025-09-18 23:00:16 +0530

    TestNG

--- src/main/java/com/automation/pages/DayTwo.java
+++ src/main/java/com/automation/pages/DayTwo.java
@@ -1,12 +1,18 @@
 package com.automation.pages;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class DayTwo {
-	
+
 	@Test
 	public void personalLoad() {
 		System.out.println("good");
+	}
+
+	@BeforeTest
+	public void beforeMethod() {
+		System.out.println("before method");
 	}
 
 }
