93396de605454d15c93faf41eefa41e8e3a6d637
ajaajahmad <ajaaj75@gmail.com>
2025-09-18 23:20:13 +0530

    TestNG

--- src/main/java/com/automation/pages/DayThree.java
+++ src/main/java/com/automation/pages/DayThree.java
@@ -1,5 +1,9 @@
 package com.automation.pages;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
@@ -9,17 +13,37 @@ public class DayThree {
 	public void webLoginCarLoan() {
 		System.out.println("web login car");
 	}
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("before class in class 3");
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		System.out.println("after class in class 3");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("before method in class 3");
+	}
+	
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("after method in class 3");
+	}
 
 	@Test
 	public void mobileLoginCarLoan() {
 		System.out.println("mobile login car");
 	}
-	
+
 	@BeforeSuite
 	public void beforeSuite() {
 		System.out.println("before suite");
 	}
-	
+
 	@Test
 	public void loginAPICarLoad() {
 		System.out.println("login api car");
