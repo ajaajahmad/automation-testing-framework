8c240c8b0c45db69eef918fc5de3b68cf5d9278c
ajaajahmad <ajaaj75@gmail.com>
2025-09-21 15:50:23 +0530

    added config utils class

--- src/main/java/com/automation/utils/ConfigUtils.java
+++ src/main/java/com/automation/utils/ConfigUtils.java
@@ -1,14 +1,11 @@
 package com.automation.utils;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigUtils {
 
 	private static Properties properties;
-	private static final String CONFIG_FILE_PATH = "src/test/resources/config.properties";
 
 	static {
 		loadProperties();
@@ -17,44 +14,20 @@ public class ConfigUtils {
 	private static void loadProperties() {
 		try {
 			properties = new Properties();
-
 			InputStream inputStream = ConfigUtils.class.getClassLoader().getResourceAsStream("config.properties");
-
-			if (inputStream != null) {
-				properties.load(inputStream);
-				System.out.println("Properties loaded from class path");
-			} else {
-				FileInputStream fileInput = new FileInputStream(CONFIG_FILE_PATH);
-				properties.load(fileInput);
-				System.out.println("Properties loaded from file path");
-				fileInput.close();
-			}
-		} catch (IOException e) {
-			System.out.println("Failed to load properties file: " + e.getMessage());
-			throw new RuntimeException("Cannot load configuration properties", e);
+			properties.load(inputStream);
+			inputStream.close();
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to load config.properties", e);
 		}
-
 	}
 
 	public static String getProperty(String key) {
-
-		String value = properties.getProperty(key);
-		if (value == null) {
-			System.out.println("Property not found: " + key);
-		}
-		return value;
-	}
-
-	public static String getProperty(String key, String defaultValue) {
-		return properties.getProperty(key, defaultValue);
+		return properties.getProperty(key);
 	}
 
 	public static String getLoginUrl() {
 		return getProperty("app.login.url");
-	}
-
-	public static String getBaseUrl() {
-		return getProperty("app.base.url");
 	}
 
 	public static String getUsername() {
@@ -66,14 +39,10 @@ public class ConfigUtils {
 	}
 
 	public static int getImplicitTimeout() {
-		return Integer.parseInt(getProperty("browser.timeout.implicit", "15"));
-	}
-
-	public static int getExplicitTimeout() {
-		return Integer.parseInt(getProperty("browser.timeout.explicit", "30"));
+		return Integer.parseInt(getProperty("browser.timeout.implicit"));
 	}
 
 	public static int getPageTimeout() {
-		return Integer.parseInt(getProperty("browser.timeout.page", "60"));
+		return Integer.parseInt(getProperty("browser.timeout.page"));
 	}
 }
