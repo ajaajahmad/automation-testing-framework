f237e616d2f84845a9b2f1844312e37806dbde10
ajaajahmad <ajaaj75@gmail.com>
2025-09-22 23:19:03 +0530

    Framework Building

--- src/main/java/com/automation/utils/DriverManager.java
+++ src/main/java/com/automation/utils/DriverManager.java
@@ -12,59 +12,59 @@ import org.openqa.selenium.safari.SafariDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverManager {
-    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-    private static Logger logger = new Logger();
+	private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-    public DriverManager() {
+	public DriverManager() {
 
-    }
+	}
 
-    public static WebDriver getDriver() {
-        if (driver.get() == null) {
-            initializeDriver();
-        }
-        return driver.get();
-    }
+	public static WebDriver getDriver() {
+		if (driver.get() == null) {
+			initializeDriver();
+		}
+		return driver.get();
+	}
 
-    public static void initializeDriver() {
-        String browser = ConfigUtils.getProperty("browser").toLowerCase();
-        boolean headless = Boolean.parseBoolean(ConfigUtils.getProperty("headless"));
+	public static void initializeDriver() {
+		String browser = ConfigUtils.getProperty("browser").toLowerCase();
+		boolean headless = Boolean.parseBoolean(ConfigUtils.getProperty("headless"));
 
-        switch (browser) {
-            case "chrome":
-                ChromeOptions chromeOptions = new ChromeOptions();
-                if (headless) chromeOptions.addArguments("--headless=new");
-                WebDriverManager.chromedriver().setup();
-                driver.set(new ChromeDriver(chromeOptions));
-                break;
-            case "firefox":
-                FirefoxOptions firefoxOptions = new FirefoxOptions();
-                if (headless) firefoxOptions.addArguments("--headless");
-                WebDriverManager.firefoxdriver().setup();
-                driver.set(new FirefoxDriver(firefoxOptions));
-                break;
-            case "edge":
-                EdgeOptions edgeOptions = new EdgeOptions();
-                if (headless) edgeOptions.addArguments("--headless");
-                WebDriverManager.edgedriver().setup();
-                driver.set(new EdgeDriver(edgeOptions));
-                break;
-            case "safari":
-                driver.set(new SafariDriver());
-                break;
-            default:
-                throw new RuntimeException("Browser " + browser + " not supported");
-        }
+		switch (browser) {
+		case "chrome":
+			ChromeOptions chromeOptions = new ChromeOptions();
+			if (headless)
+				chromeOptions.addArguments("--headless=new");
+			WebDriverManager.chromedriver().setup();
+			driver.set(new ChromeDriver(chromeOptions));
+			break;
+		case "firefox":
+			FirefoxOptions firefoxOptions = new FirefoxOptions();
+			if (headless)
+				firefoxOptions.addArguments("--headless");
+			WebDriverManager.firefoxdriver().setup();
+			driver.set(new FirefoxDriver(firefoxOptions));
+			break;
+		case "edge":
+			EdgeOptions edgeOptions = new EdgeOptions();
+			if (headless)
+				edgeOptions.addArguments("--headless");
+			WebDriverManager.edgedriver().setup();
+			driver.set(new EdgeDriver(edgeOptions));
+			break;
+		case "safari":
+			driver.set(new SafariDriver());
+			break;
+		default:
+			throw new RuntimeException("Browser " + browser + " not supported");
+		}
 
-        logger.info("Initialized " + browser + " browser" + (headless ? " in headless mode" : ""));
-        driver.get().manage().window().maximize();
-    }
+		driver.get().manage().window().maximize();
+	}
 
-    public static void quitDriver() {
-        if (driver.get() != null) {
-            driver.get().quit();
-            driver.remove();
-            logger.info("Closed browser session");
-        }
-    }
+	public static void quitDriver() {
+		if (driver.get() != null) {
+			driver.get().quit();
+			driver.remove();
+		}
+	}
 }
