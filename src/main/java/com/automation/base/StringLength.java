b65da8fee88b5ae49d07d42ff577e946a02b373d
ajaajahmad <ajaaj75@gmail.com>
2025-09-23 21:36:17 +0530

    changes

--- src/main/java/com/automation/base/StringLength.java
+++ src/main/java/com/automation/base/StringLength.java
@@ -17,6 +17,16 @@ public class StringLength {
 		reversed = reversed.trim();
 
 		System.out.println(reversed);
+		
+		String str2 = "how are you";
+
+		int count = 0;
+		char[] arr = str2.toCharArray();
+		for (int i = 0; i < arr.length; i++) {
+			count++;
+		}
+		
+		System.out.println(count);
 	}
 
 }
