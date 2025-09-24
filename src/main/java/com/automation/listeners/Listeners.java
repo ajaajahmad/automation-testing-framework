27177f4400446104ffe22f5c94d2c4bd069724ec
ajaajahmad <ajaaj75@gmail.com>
2025-09-24 23:37:17 +0530

    Listeners

--- src/main/java/com/automation/listeners/Listeners.java
+++ src/main/java/com/automation/listeners/Listeners.java
@@ -4,28 +4,29 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class Listeners implements ITestListener{
-	
+public class Listeners implements ITestListener {
+
 	@Override
-	public void onStart(ITestContext context) {
-		// TODO Auto-generated method stub
-		ITestListener.super.onStart(context);
+	public void onTestStart(ITestResult result) {
+
 	}
-	
+
 	@Override
 	public void onTestFailure(ITestResult result) {
-		// TODO Auto-generated method stub
-		ITestListener.super.onTestFailure(result);
+
 	}
-	
+
 	@Override
 	public void onTestSuccess(ITestResult result) {
-		// TODO Auto-generated method stub
-		ITestListener.super.onTestSuccess(result);
+
 	}
+
 	@Override
-	public void onFinish(ITestContext context) {
-		// TODO Auto-generated method stub
-		ITestListener.super.onFinish(context);
+	public void onTestSkipped(ITestResult result) {
+
+	}
+
+	public void onTestFinish(ITestResult result) {
+
 	}
 }
