c1f165ddb24bd7eaf532e64a9ab67cf1b762ad29
ajaajahmad <ajaaj75@gmail.com>
2025-09-25 23:51:35 +0530

    changes

--- src/main/java/com/automation/utils/AuthUtils.java
+++ src/main/java/com/automation/utils/AuthUtils.java
@@ -33,7 +33,7 @@ public class AuthUtils {
 
 	public static boolean login(WebDriver driver, String username, String password) {
 		try {
-			System.out.println("Attempting login");
+			System.out.println("Attempting login with username: " + username);
 
 			AuthUtils auth = new AuthUtils(driver);
 
