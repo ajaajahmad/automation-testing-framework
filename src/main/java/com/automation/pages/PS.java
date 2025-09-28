40e7b75cc613e13bb6c9e0bae850a24d6b005507
ajaajahmad <ajaaj75@gmail.com>
2025-09-28 15:15:19 +0530

    OOPS

--- src/main/java/com/automation/pages/PS.java
+++ src/main/java/com/automation/pages/PS.java
@@ -1,17 +1,23 @@
 package com.automation.pages;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class PS {
-	
-	//methods, variables
+
+	// methods, variables
 	public void doThis() {
-		
+
 		System.out.println("i am here");
 	}
-	
+
 	@BeforeMethod
 	public void beforeRun() {
 		System.out.println("run me first");
+	}
+
+	@AfterMethod
+	public void afetrRun() {
+		System.out.println("run me last");
 	}
 }
