cabed790f520bfd8123444e7fad61b3848af9b6b
ajaajahmad <ajaaj75@gmail.com>
2025-09-28 15:31:31 +0530

    OOPS

--- src/main/java/com/automation/pages/PS1.java
+++ src/main/java/com/automation/pages/PS1.java
@@ -6,7 +6,13 @@ public class PS1 extends PS {
 
 	@Test
 	public void runTest() {
+
+		PS2 ps2 = new PS2(3);
+
 		int a = 3;
 		doThis();
+
+		System.out.println(ps2.increament());
+		System.out.println(ps2.decreament());
 	}
 }
--- src/main/java/com/automation/pages/PS2.java
+++ src/main/java/com/automation/pages/PS2.java
@@ -4,8 +4,17 @@ public class PS2 {
 
 	int a;
 
+	public PS2(int a) {
+		this.a = a;
+	}
+
 	public int increament() {
 		a = a + 1;
+		return a;
+	}
+
+	public int decreament() {
+		a = a - 1;
 		return a;
 	}
 }
