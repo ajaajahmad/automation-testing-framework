e09b47c8d9bac7cfa3a08788ff5e5d49dc599a40
ajaajahmad <ajaaj75@gmail.com>
2025-09-28 22:45:20 +0530

    OOPS

---
+++ src/main/java/com/automation/pages/PS3.java
@@ -0,0 +1,16 @@
+package com.automation.pages;
+
+public class PS3 {
+
+	int a;
+
+	public int multiplyTwo() {
+		a = a * 2;
+		return a;
+	}
+
+	public int multiplyThree() {
+		a = a * 3;
+		return a;
+	}
+}
