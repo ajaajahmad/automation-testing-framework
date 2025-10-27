c43a9e47dc7197daa173fc2ebfaa4dbd997fbcd3
ajaajahmad <ajaaj75@gmail.com>
2025-10-27 22:10:30 +0530

    changes

--- src/main/java/com/automation/base/XpathWithIndex.java
+++ src/main/java/com/automation/base/XpathWithIndex.java
@@ -1,30 +1,42 @@
 package com.automation.base;
 
 import java.time.Duration;
-import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class XpathWithIndex {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
-		FluentWait<WebDriver> wait = new FluentWait<>(driver).withTimeout(Duration.ofSeconds(20))
-				.pollingEvery(Duration.ofSeconds(2)).ignoring(NoSuchElementException.class);
+        // Create WebDriverWait instance (Explicit Wait)
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
-		driver.get("https://www.fantasticfurniture.com.au/");
-		WebElement menu = wait.until(drv -> drv.findElement(By.xpath("//nav[@aria-label='menuItem'][2]/a")));
-		menu.click();
-		WebElement submenu = wait.until(drv -> drv.findElement(By.xpath("(//div[@class='desktop-submenu'])[1]//ul/li[2]/a")));
-		submenu.click();
-		driver.quit();
-	}
+        driver.get("https://www.fantasticfurniture.com.au/");
 
+        // Wait for and hover over the 2nd menu item
+        WebElement menu = wait.until(ExpectedConditions.visibilityOfElementLocated(
+            By.xpath("(//nav[@aria-label='menuItem'])[2]/a")
+        ));
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(menu).perform();
+
+        // Wait until submenu is visible and click on "King Single Beds"
+        WebElement submenu = wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//a[normalize-space()='King Single Beds']")
+        ));
+
+        submenu.click();
+
+        driver.quit();
+    }
 }
