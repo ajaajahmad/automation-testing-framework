517b3cfbc751be1597e2a81bf6dc8316bc4bf551
ajaajahmad <ajaaj75@gmail.com>
2025-11-02 22:52:15 +0530

    implicit wait

--- src/main/java/com/automation/base/ImplicitWait.java
+++ src/main/java/com/automation/base/ImplicitWait.java
@@ -3,12 +3,14 @@ package com.automation.base;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ImplicitWait {
 
 	public static void main(String[] args) {
 
-		WebDriver driver = null;
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
 
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 	}
