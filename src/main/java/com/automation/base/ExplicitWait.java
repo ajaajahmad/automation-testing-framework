24a1524753b062bc75b61e423e58ea60fc28a9b9
ajaajahmad <ajaaj75@gmail.com>
2025-11-02 22:51:30 +0530

    explicit wait

--- src/main/java/com/automation/base/ExplicitWait.java
+++ src/main/java/com/automation/base/ExplicitWait.java
@@ -3,13 +3,15 @@ package com.automation.base;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ExplicitWait {
 
 	public static void main(String[] args) {
 
-		WebDriver driver = null;
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
 	}
 
