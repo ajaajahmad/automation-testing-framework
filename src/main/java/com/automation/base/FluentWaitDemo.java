9a9f99fa90ced8244d3886b797d24643f5c21d34
ajaajahmad <ajaaj75@gmail.com>
2025-11-02 23:00:52 +0530

    fluent wait

---
+++ src/main/java/com/automation/base/FluentWaitDemo.java
@@ -0,0 +1,22 @@
+package com.automation.base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+public class FluentWaitDemo {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+
+		Wait<WebDriver> wait = new FluentWait<>(driver).withTimeout(Duration.ofSeconds(10))
+				.pollingEvery(Duration.ofSeconds(3)).ignoring(NoSuchElementException.class);
+	}
+
+}
