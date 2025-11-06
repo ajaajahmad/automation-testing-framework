56a0f943f5ce70e8811d93cfe43a02b472c45a30
ajaajahmad <ajaaj75@gmail.com>
2025-11-07 00:00:03 +0530

    dynamic xpath

--- src/main/java/com/automation/base/DynamicXpath.java
+++ src/main/java/com/automation/base/DynamicXpath.java
@@ -9,41 +9,50 @@ public class DynamicXpath {
 	public static void main(String[] args) {
 
 		WebDriver driver = new ChromeDriver();
-		
-		driver.get("http://www.facebook.com");
+
+		driver.get("https://www.facebook.com");
+
+		// --- Basic attribute-based XPaths ---
 		By.xpath("//input[@name='email']");
-		By.xpath("//input[@id=’email’]");
+		By.xpath("//input[@id='email']");
 		By.xpath("//input[@type='email']");
-		By.xpath("//input[@class=’inputtext’]");
-		By.xpath("//*[@class=’inputtext’]");
-		By.xpath("//input[@class=’inputtext’][@type='email']");
-		
-		// Basic
+		By.xpath("//input[@class='inputtext']");
+		By.xpath("//*[@class='inputtext']");
+		By.xpath("//input[@class='inputtext' and @type='email']");
+
+		// --- Basic link ---
 		By.xpath("//a[@href='https://www.facebook.com/']");
-		// Contains
+
+		// --- contains() function ---
 		By.xpath("//*[contains(@name,'passwd_')]");
-		// AND & OR
+
+		// --- Logical operators: AND / OR ---
 		By.xpath("//*[@class='inputtext' or @name='email']");
 		By.xpath("//*[@class='inputtext' and @name='email']");
-		// Start-with
+
+		// --- starts-with() function ---
 		By.xpath("//*[starts-with(@id,'yui_3_18_0_3_1555510217471_')]");
-		// Text
-		By.xpath("//*[text(),"Create an account"]");
-		// Last
-		By.xpath("//input[@type='text'])[last()]");
-		// Position
-		By.xpath("//input[@type='text'])[2]");
-		// Following
+
+		// --- text() function ---
+		By.xpath("//*[text()='Create new account']");
+
+		// --- last() function ---
+		By.xpath("(//input[@type='text'])[last()]");
+
+		// --- position() / index ---
+		By.xpath("(//input[@type='text'])[2]");
+
+		// --- following axis ---
 		By.xpath("//*[@name='firstname']");
 		By.xpath("//*[@name='firstname']//following::input[@type='text']");
-		By.xpath("//*[@name=’firstname’]//following::input[1]");
-		// Preceding
+		By.xpath("//*[@name='firstname']//following::input[1]");
+
+		// --- preceding axis ---
 		By.xpath("//*[@name='lastname']");
 		By.xpath("//*[@name='lastname']//preceding::input[1]");
-		By.xpath("[@name='lastname']//preceding::input[@type='text']");
-		
-		
-		
-	}
+		By.xpath("//*[@name='lastname']//preceding::input[@type='text']");
 
+		// Close browser
+		driver.quit();
+	}
 }
