6c887488021cdfd503ed5b1cdd1107bd9e0df789
Ajaaj Ahmad <57870490+ajaajahmad@users.noreply.github.com>
2025-11-15 23:30:35 +0530

    Count characters in a string using a loop

--- src/main/java/com/automation/base/StringLength.java
+++ src/main/java/com/automation/base/StringLength.java
@@ -17,8 +17,11 @@ public class StringLength {
 		System.out.println(reversed);
 
 		String str2 = "how are you";
+		
 		int count = 0;
+		
 		char[] arr = str2.toCharArray();
+		
 		for (int i = 0; i < arr.length; i++) {
 			count++;
 		}
