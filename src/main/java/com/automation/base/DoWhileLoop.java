bf90eedca25172f5f5decbe2d907a1c7e2e668c0
ajaajahmad <ajaaj75@gmail.com>
2025-11-16 22:54:25 +0530

    do while loop

---
+++ src/main/java/com/automation/base/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package com.automation.base;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+
+		int i = 20;
+
+		do {
+			System.out.println(i);
+			i++;
+		} while (i < 30); //execution of 1 loop is guarantee.
+	}
+
+}
