32f4d02fd0fa7b1423d2c9ec68b59210fe6595d4
ajaajahmad <ajaaj75@gmail.com>
2025-11-17 22:36:39 +0530

    nested loop

---
+++ src/main/java/com/automation/base/NestedLoopPractical3.java
@@ -0,0 +1,16 @@
+package com.automation.base;
+
+public class NestedLoopPractical3 {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j);
+				System.out.print("\t");
+			}
+			System.out.println("");
+		}
+	}
+
+}
