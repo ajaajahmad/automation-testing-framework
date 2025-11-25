8078ff55bc452dc9e6439eb564436b3e78fa7d9d
ajaajahmad <ajaaj75@gmail.com>
2025-11-26 00:15:54 +0530

    static non static

--- src/main/java/com/automation/base/StaticAndNonStatic.java
+++ src/main/java/com/automation/base/StaticAndNonStatic.java
@@ -4,23 +4,25 @@ public class StaticAndNonStatic {
 
 	String name;
 	String address;
-	String city;
+	static String city = "Mumbai";
+	static int i = 0;
 
-	StaticAndNonStatic(String name, String address, String city) {
+	StaticAndNonStatic(String name, String address) {
 
 		this.name = name;
 		this.address = address;
-		this.city = city;
+		i++;
+		System.out.println(i);
 	}
 
 	public void getAddress() {
-		System.out.println(address);
+		System.out.println(address + " " + city);
 	}
 
 	public static void main(String[] args) {
 
-		StaticAndNonStatic st = new StaticAndNonStatic("Ajaaj", "Mumbra", "Mumbai");
-		StaticAndNonStatic st2 = new StaticAndNonStatic("Ahmad", "Kausa", "Mumbai");
+		StaticAndNonStatic st = new StaticAndNonStatic("Ajaaj", "Mumbra");
+		StaticAndNonStatic st2 = new StaticAndNonStatic("Ahmad", "Kausa");
 		st2.getAddress();
 		st.getAddress();
 	}
