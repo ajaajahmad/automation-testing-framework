a0691cd1ea03feff11af5269e4517eb9b6ce2b10
ajaajahmad <ajaaj75@gmail.com>
2025-12-02 18:29:04 +0530

    dropdown practices

--- src/main/java/com/automation/base/DropDownPractices.java
+++ src/main/java/com/automation/base/DropDownPractices.java
@@ -1,11 +1,14 @@
 package com.automation.base;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DropDownPractices {
@@ -19,11 +22,17 @@ public class DropDownPractices {
 
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
-		WebElement countryOption = driver.findElement(By.xpath("//div[contains(text(), 'Select Country')]"));
+		By dropdownLocator = By.xpath("//div[contains(text(), 'Select Country')]");
 
-		countryOption.click();
-		
-		
+		WebElement dropdown = wait.until(ExpectedConditions.elementToBeClickable(dropdownLocator));
+
+		dropdown.click();
+
+		By itemLocator = By.cssSelector(".menu .item");
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(itemLocator));
+
+		List<WebElement> allListItem = dropdown.findElements(itemLocator);
 
 		driver.quit();
 	}
