42a24d04f7ac1014cdacc0eb15177fe306845013
ajaajahmad <ajaaj75@gmail.com>
2025-12-02 18:00:11 +0530

    dropdown practices

---
+++ src/main/java/com/automation/base/DropDownPractices.java
@@ -0,0 +1,29 @@
+package com.automation.base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DropDownPractices {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://semantic-ui.com/modules/dropdown.html");
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+		WebElement countryOption = driver.findElement(By.xpath("//div[contains(text(), 'Select Country')]"));
+
+		countryOption.click();
+
+		driver.quit();
+	}
+
+}
--- src/main/java/com/automation/base/DynamicDropDown.java
+++ src/main/java/com/automation/base/DynamicDropDown.java
@@ -1,9 +1,11 @@
 package com.automation.base;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DynamicDropDown {
@@ -14,17 +16,36 @@ public class DynamicDropDown {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		List<WebElement> options = driver.findElements(By.id("ctl00_mainContent_ddl_originStation1"));
+		WebElement dropDown = driver.findElement(By.id("ctl00_mainContent_ddl_originStation1"));
+		
+		String tagName = dropDown.getTagName();
+		
+		if(!tagName.equalsIgnoreCase("select")) {
+			System.out.println("Dropdown is a <select> tag");
+		}else {
+			System.out.println("Dropdown is not a <select> tag");
+		}
+		
+		if(options.size()>0) {
+			System.out.println("Dropdown is empty");
+		}else {
+			System.out.println("Dropdown is not empty");
+		}
+		
 
-		driver.findElement(By.cssSelector("#ctl00_mainContent_ddl_originStation1_CTXT")).click();
-		Thread.sleep(2000);
-		// driver.findElement(By.xpath("//a[@value='MAA']")).click();
-		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='MAA']"))
-				.click();
-
-		// driver.findElement(By.xpath("(//a[@value='BLR'])[2]")).click();
-		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='BLR']"))
-				.click();
-		Thread.sleep(2000);
+//		driver.findElement(By.cssSelector("#ctl00_mainContent_ddl_originStation1_CTXT")).click();
+//		
+//		Thread.sleep(2000);
+//		// driver.findElement(By.xpath("//a[@value='MAA']")).click();
+//		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='MAA']"))
+//				.click();
+//
+//		// driver.findElement(By.xpath("(//a[@value='BLR'])[2]")).click();
+//		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='BLR']"))
+//				.click();
+//		Thread.sleep(2000);
 		driver.quit();
 	}
 
